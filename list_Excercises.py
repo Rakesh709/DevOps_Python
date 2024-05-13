dc91f66939ae1d6139c4d068da2c760e2f55c8f3
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-14 01:00:43 +0530

    list excersises

---
+++ list_Excercises.py
@@ -0,0 +1,60 @@
+#https://www.geeksforgeeks.org/python-exercises-practice-questions-and-solutions/
+
+#Python program to interchange first and last elements in a list
+
+
+
+# def swapList(newList):
+#     size= len(newList)
+#     # print(size)
+#     temp= newList[0]
+#     newList[0]=newList[size-1]
+#     newList[size-1]=temp
+#     return newList
+
+
+# print(swapList([1,2,3,4]))
+
+# def swaplist(newList):
+#     newList[0],newList[-1]=newList[-1],newList[0]
+
+#     return newList
+
+# print(swaplist([1,2,3,4]))
+
+
+#-----------------------------------------------------------------------------------
+
+#Python Program to Swap Two Elements in a List
+#not getting 
+
+
+#-------------------------------------------
+#How To Find the Length of a List in Python
+
+# def lenght_Finder(arr):
+#     n= len(arr)
+#     return n
+
+# print(lenght_Finder([1,2,3,4,5]))
+
+
+# def length_finder(arr):
+#     count=0
+#     for i in arr:
+#         count+=1
+#     return count
+    
+# print(length_finder([1,2,3,4,5]))
+
+
+#-------------------------------------------------
+#Find Maximum of two numbers in Python
+
+# def max_finder(num1,num2):
+#     if num1>num2:
+#         return "num1 is bigger"
+#     else:
+#         return "num2 is bigger"
+
+# print(max_finder(-9,-6))
