0c86ec5216f5a5e961244abf5c1ac40732b6fadd
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-13 23:17:55 +0530

    Python Basics

---
+++ app.py
@@ -0,0 +1,95 @@
+# print("Hello World");
+
+# age=20
+# price = 19.96
+# first_name='rakesh'
+# is_online= False
+# print(age)
+
+# patient_name= "john smith"
+# age=20
+
+# --------
+
+# name=input("what is your name ")
+# print("hello " + name)
+
+#-------------------
+
+# birth_year = input("Enter your birth year: ")
+# age = 2020 - int(birth_year)
+# print(age)
+
+# num1= float(input("first: "))
+# num2= float(input("second: "))
+# print(num1+num2)
+
+#-------------------
+#string
+
+# course = "Python for bignners"
+# print(course.upper())
+# print(course.find('y')) #return the first occuring index 
+# print(course.replace("for","4"))
+# print("Python" in course)
+
+#----------------
+#Arithmatic operator
+
+#print(10/3) 
+#3.3333333333333335
+#print(10//3)
+#3
+#print(10%3) #reminder 1
+
+# print(10**3)
+
+#ogumented assigned operator
+
+# x=3
+# # x=x+3
+# x+=3  # agumented assigned operator
+# print(x)
+
+#----------------------
+
+#operator precedence
+
+#bodmas
+# x=10+3*2
+# print(x)
+
+#------------------
+#comparision operator
+
+# x=3>2
+# x=3==2
+# x= 3!=2
+# print(x)
+
+# >
+# >=
+# <
+# <=
+# ==
+# !=
+
+#--------------------------
+#logical operator
+
+# price=25
+# print(price>10 and price<30)
+# True
+
+# price=5
+# print(price>10 or price<30)
+# True
+
+# price =5
+# print(not price>10)
+#True
+
+
+# and (both)
+# or (at least one)
+# not (opposite)
