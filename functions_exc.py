547b9d95d47aa99d955bd76dba5626105e13e11c
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-15 23:54:27 +0530

    functions

---
+++ functions_exc.py
@@ -0,0 +1,47 @@
+#defaulr argument 
+
+# def greeting(msg="good morning"):
+#     print(msg)
+
+# greeting()
+
+# order of argument
+ 
+#argument need to pass i  proper order
+
+
+# vac_feedback(effeccancy=45,vac="unkown")
+
+# in the ABOVE WE CAN PASSS IN ANY ORDER NOW 
+
+
+#------------------------------------------------------------------
+
+#varibale length argumennt *args ( non keyword argument)
+
+
+# def order_food(min_order,*args):
+#     print("your order: ",min_order)
+#     for item in args:
+#         print("your order:",item)
+
+
+
+# order_food("cake","coke","samosa")
+
+#-------------------------------------------------------------------
+
+
+#varibale length argumennt *kwargs (keyword argument)
+
+
+
+
+
+
+
+
+
+
+
+
