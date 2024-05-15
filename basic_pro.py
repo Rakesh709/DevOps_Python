8487beda1605a6be1b0768496be85c968ae41e72
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-15 06:19:26 +0530

    basics programming

---
+++ basic_pro.py
@@ -0,0 +1,45 @@
+#https://www.geeksforgeeks.org/python-programming-examples/
+
+#Python Program for factorial of a number
+
+#3!= 3*2*1
+
+# def factorial(n):
+#     return 1 if(n==0 or n==1) else n*factorial(n-1)
+
+# print(factorial(3))
+
+#-------------------------------
+
+#prime number
+
+# num=5
+
+# for i in range(2,num):
+#     if num%i==0:
+#         print("not prime")
+#         break
+# else:
+#     print('prime number')
+
+#-------------------------------------
+
+#palindrome number
+
+# name="madam"
+# name = input("enter the value: ")
+# reverse =name[::-1]
+
+# if(name==reverse):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+#-----------------------------------
+
+
+# name = input("enter the value: ")
+# reverse =name[::-1]
+# print(reverse)
+
