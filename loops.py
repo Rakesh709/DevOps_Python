7d3f4dee3bb9b3cf514238d997c7bc9f75f9a646
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-15 23:29:23 +0530

    loop concept

---
+++ loops.py
@@ -0,0 +1,65 @@
+#for loop : iterate over sequence
+
+# Planet = "Earth"
+
+# for i in Planet:
+#     print(i)
+
+
+# while loop iterate over condition
+
+# while loop
+
+# x=0 
+
+# while x<=10:
+#     print(x)
+#     x+=1
+
+#----------------------------------------------------
+#nested loop
+sports =["cricket","football","basketball"]
+
+# for s in sports:
+#     print("i love to play",s)
+
+#     for i in s:
+#         print(i)
+
+
+#while loop
+
+# x=2
+
+# while x<=100:
+#     print(x)
+#     x*=2
+
+#-------------
+#2 table 
+
+# for i in range(1,11):
+#     print(i*3)
+
+
+#------------------------------------------------------------
+#break
+
+# for i in "devops":
+#     print(i)
+#     if i =="v":
+#         print("found it")
+#         break
+# print("out")
+
+
+#continue
+
+# for i in "devops":
+#     if i=="v":
+#         print("found my data")
+#         continue
+#     print(i)
+
+# print("out")
+    
