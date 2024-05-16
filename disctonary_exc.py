702197f3f5e190c1760780e031d36c99fd597a51
Rakesh709 <kumar.rakesh1997@outlook.com>
2024-05-17 00:29:08 +0530

    dict

---
+++ disctonary_exc.py
@@ -0,0 +1,53 @@
+#sum of dict
+
+# dict = {'a': 100, 'b': 200, 'c': 300}
+
+# sum =0
+
+# for i in dict.values():
+#     sum+=i
+# print(sum)
+
+#------------------------------
+
+#Merging two Dictionaries
+
+# def merge(dict1,dict2):
+#     return(dict2.update(dict1))
+
+# dictt1 = {'a': 10, 'b': 8}
+# dictt2= {'d': 6, 'c': 4}
+
+
+# print(merge(dictt1,dictt2))
+
+# print(dictt2)
+
+
+#----------------------------------------------
+
+#commonmethod in dictonary
+
+
+my_dict = {'name': 'Alice', 'age': 25}
+
+# print(my_dict)
+# print(my_dict["name"])
+# print(my_dict['age'])
+
+# Get the value of the 'name' key
+
+# name = my_dict.get("name")
+# print(name)
+
+
+
+## Get all the items in the dictionary
+# item = my_dict.items()
+# print(item)
+
+# Print the items
+
+# for key,value in item:
+#     print(key,value)
+
